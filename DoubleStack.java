05fcb73ba2dc8ef3d7a877e8085b8baed64ce213
mainrid <mainrid@gmail.com>
2016-03-10 23:45:42 +0200

    final method "interpret" implemented

--- DoubleStack.java
+++ DoubleStack.java
@@ -1,116 +1,148 @@
 import java.util.Stack;
+import java.util.StringTokenizer;
+
+import javax.management.RuntimeErrorException;
 
 public class DoubleStack {
 	private Stack<Double> stack;
 
-   public static void main (String[] argum) {
-      DoubleStack first= new DoubleStack();
-      first.push(3.5);
-      first.push(4.5);
-      
-      try {
-		DoubleStack firstClone= (DoubleStack)first.clone();
-		System.out.println(first.equals(firstClone));
-	} catch (CloneNotSupportedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public static void main(String[] argum) {
+		DoubleStack first = new DoubleStack();
+		first.push(3.5);
+		first.push(4.5);
+
+		try {
+			DoubleStack firstClone = (DoubleStack) first.clone();
+			System.out.println(first.equals(firstClone));
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-            
-   }
 
-   DoubleStack() {
-	   this.stack= new Stack<Double>();
-   }
+	DoubleStack() {
+		this.stack = new Stack<Double>();
+	}
 
-   @Override
-   public Object clone() throws CloneNotSupportedException {
-	   DoubleStack temporary = new DoubleStack();
-	   DoubleStack clone = new DoubleStack();
-	   
-	   int stackSize= this.stack.size();
-	   for (int i = 0; i < stackSize; i++) {
-		   double temp = this.stack.pop();
-		   temporary.push(temp);
-	   }
-	   for (int i = 0; i < stackSize; i++) {
-		   double temp = temporary.pop();
-		   clone.push(temp);	
-		   this.stack.push(temp);
-		   	
-	   }
-	   
-	   return clone;
-	   
-   }
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		DoubleStack temporary = new DoubleStack();
+		DoubleStack clone = new DoubleStack();
 
-   public boolean stEmpty() {
-	  if(this.stack.isEmpty()){
-		  return true;
-	  } 
-      return false; 
-   }
+		int stackSize = this.stack.size();
+		for (int i = 0; i < stackSize; i++) {
+			double temp = this.stack.pop();
+			temporary.push(temp);
+		}
+		for (int i = 0; i < stackSize; i++) {
+			double temp = temporary.pop();
+			clone.push(temp);
+			this.stack.push(temp);
 
-   public void push (double a) {
-      this.stack.push(a);
-	   
-   }
+		}
 
-   public double pop() {
-      return this.stack.pop();
-   } // pop
+		return clone;
 
-   public void op (String s) {
-      double first = pop();
-      double second = pop();
-      
-      if(s.equals("+")){
-    	  push(second+first);
-      }else if(s.equals("-")){
-    	  push(second-first);
-      }else if(s.equals("*")){
-    	  push(second*first);
-      }else if(s.equals("/")){
-    	  push(second/first);
-      }
-   }
-  
-   public double tos() {
-      return stack.peek(); 
-   }
+	}
 
-   @Override
-   public boolean equals (Object o) {
-	  if(o==null)
-		  return false;
-	  if(!(o instanceof DoubleStack)) 
-		  return false;
-	  
-	  DoubleStack other = (DoubleStack)o;
-	  if(this.stack.equals(other.stack))
-		  return true;
-	  
-      return false;
-   }
+	public boolean stEmpty() {
+		if (this.stack.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
 
-   @Override
-   public String toString() {
-	   if (stEmpty()){
-		   return "Stack is empty";
-	   }
-	   
-      StringBuffer stackData = new StringBuffer();
-      
-      for (int i = 0; i < stack.size(); i++) {
-    	  stackData.append(stack.get(i).toString() + " ");
-      }
-      return stackData.toString();
-      
-	   
-   }
+	public void push(double a) {
+		this.stack.push(a);
 
-   public static double interpret (String pol) {
-      return 0.; // TODO!!! Your code here!
-   }
+	}
+
+	public double pop() {
+		return this.stack.pop();
+	} // pop
+
+	public void op(String s) {
+		double first = pop();
+		double second = pop();
+
+		if (s.equals("+")) {
+			push(second + first);
+		} else if (s.equals("-")) {
+			push(second - first);
+		} else if (s.equals("*")) {
+			push(second * first);
+		} else if (s.equals("/")) {
+			push(second / first);
+		}
+	}
+
+	public double tos() {
+		return stack.peek();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null)
+			return false;
+		if (!(o instanceof DoubleStack))
+			return false;
+
+		DoubleStack other = (DoubleStack) o;
+		if (this.stack.equals(other.stack))
+			return true;
+
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		if (stEmpty()) {
+			return "Stack is empty";
+		}
+
+		StringBuffer stackData = new StringBuffer();
+
+		for (int i = 0; i < stack.size(); i++) {
+			stackData.append(stack.get(i).toString() + " ");
+		}
+		return stackData.toString();
+
+	}
+
+	public int getSize() {
+		return this.stack.size();
+	}
+
+	public static double interpret(String pol) {
+		StringTokenizer splitted = new StringTokenizer(pol);
+		String next = "";
+		String operators = "+-/*";
+		DoubleStack stack = new DoubleStack();
+		String currentOperator = "";
+		double rpnValue = 0;
+
+		while (splitted.hasMoreTokens()) {
+			next = splitted.nextToken();
+
+			if (!operators.contains(next)) {
+				try {
+					double v = Double.valueOf(next);
+					stack.push(v);
+				} catch (NumberFormatException e) {
+					throw new RuntimeException("Invalid symbols");
+				}
+
+			} else {
+				currentOperator = next;
+				stack.op(currentOperator);
+			}
+		}
+		rpnValue = stack.pop();
+		if (!stack.stEmpty()) {
+			throw new RuntimeException("Some operands left in stack");
+		}
+		return rpnValue;
+	}
 
 }
-
